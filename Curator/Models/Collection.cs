f46b3c874f8be07969615c6f3d467a681a696169
cfbock <christopher.f.bock@gmail.com>
2026-08-19 21:16:11 -0400

    Clean up database code and improve naming
    Clean up database code and improve naming

--- Curator/Models/Collection.cs
+++ Curator/Models/Collection.cs
@@ -1,22 +1,19 @@
 ﻿using SQLite;
 
-namespace Curator.Models
-{
-    public class Collection
-    {
-        // SQLite.NET ORM attributes.
-        // Tell SQLite this property is the table's primary key
-        // and to automatically generate the next ID when a new row is added.
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+namespace Curator.Models;
 
-        // Other properties describing a collection.
-        public string Name { get; set; } = "";
-        public bool IsFolder { get; set; }
-        public string CollectionType { get; set; } = "";
-        public int? ParentCollectionId { get; set; } // Nullable to allow for root collections (not in "folders")
-        
-        [Ignore]
-        public int ItemCount { get; set; }
-    }
+public class Collection
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = "";
+    public bool IsFolder { get; set; }
+    public string CollectionType { get; set; } = "";
+
+    // Null indicates a root-level collection
+    public int? ParentCollectionId { get; set; } 
+    
+    [Ignore]
+    public int ItemCount { get; set; }
 }
