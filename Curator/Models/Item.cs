fcdf008334c487229754d1ca1de8ad49c9ea29f2
cfbock <christopher.f.bock@gmail.com>
2026-08-29 12:13:01 -0400

    Item.cs Correction
    Corrected error with missing ; after setting the property to "" instead of leaving it at null.

--- Curator/Models/Item.cs
+++ Curator/Models/Item.cs
@@ -6,6 +6,6 @@ public class Item
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
     public int CollectionId { get; set; }
-    public string Name { get; set; } = ""
+    public string Name { get; set; } = "";
 
 }
